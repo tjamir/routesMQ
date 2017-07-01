ea79f6d9da2dbc1e0304bad200219ce173a104d0
tjamir <tjamir@gmail.com>
2017-07-01 16:58:42 -0300

    Implementação do repositório de mensagens

--- src/main/java/br/ufpe/cin/routesmq/persistence/MessageRepository.java
+++ src/main/java/br/ufpe/cin/routesmq/persistence/MessageRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
  */
 public interface MessageRepository {
 
+    void addMessage(ApplicationMessage message);
+
     public List<ApplicationMessage> getMessages(PeerDestination destination);
 
 
